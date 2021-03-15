8ee1fc56cfd7ae6010a0e65293e743b23ba4fb00
David Verghese <dj.verghese@gmail.com>
2021-03-15 19:46:36 -0400

    Worked on merging in the buttons

---
+++ src/MusicMachineDiatonicButtonGrids/MusicMachineDiatonicButtonRows/MusicMachineDiatonicButtonRows.jsx
@@ -0,0 +1,14 @@
+import Button from '../../Buttons/Button.jsx';
+import './MusicMachineDiatonicButtonRows.css';
+
+function MusicMachineDiatonicButtonRows(props) {
+  // const array = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32"];
+  return (
+    <div className="buttons-row">
+    {props.array.map((index,key)=>{
+      return <Button button={index}/>
+    })}
+    </div>
+    )
+}
+export default MusicMachineDiatonicButtonRows
